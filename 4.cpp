95c47f52126b818d5c5b8facaf863c7ab9bd59a6
lyf <1289881056@qq.com>
2024-07-04 17:26:36 +0800

    md

--- 4.cpp
+++ 4.cpp
@@ -6,10 +6,12 @@ int main(){
     int a[3]={2, 3, 4};
     cout<<a[0]+a[1]+a[2]<<endl;
 
-    char name1[15];
-    char name2[15]="c++owboy";
+    char name1[3];
+    char name2[15];
     cout<<"input name1"<<endl;
-    cin>>name1;
+    cin.getline(name1, 15);
+    cout<<"input name2"<<endl;
+    cin.getline(name2, 15);
     cout<<"name1="<<name1<<endl;
     cout<<"name2="<<name2<<endl;
     name2[3]='\0';
